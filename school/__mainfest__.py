23e02d225fffa1ae681d0e995f0aa2b9c834d8b3
monaf <monaf84monaf84@gmail.com>
2021-07-24 07:32:12 -0700

    first commit

---
+++ school/__mainfest__.py
@@ -0,0 +1,13 @@
+
+{
+
+
+    'name': 'School',
+    'version': '1.0',
+    'category': 'school',
+    'summary': 'Sales Teams',
+    'description': "hfgh ",
+    'website': 'https://github.com/Monaf84DAS',
+
+
+}
